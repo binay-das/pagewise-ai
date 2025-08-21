3d0324750d807c179c8bc54ea376cf9ce8981c4e
Binay <binaydas1123@gmail.com>
2025-08-21 16:18:40 +0530

    storage bucket init

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/components/AuthProvider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-        {children}
+          <EdgeStoreProvider>{children}</EdgeStoreProvider>
         </AuthProvider>
       </body>
     </html>
