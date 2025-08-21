33e380b698aa702a814e905e634d83847d55fd9e
Binay <binaydas1123@gmail.com>
2025-08-21 16:10:07 +0530

    signup route

---
+++ app/api/signup/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import {prisma} from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { email, password } = body;
+
+    if (!email || !password) {
+      return new NextResponse("Missing email or password", { status: 400 });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUser) {
+      return new NextResponse("User with this email already exists", {
+        status: 409, 
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword, 
+      },
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+    return NextResponse.json(userWithoutPassword);
+    
+  } catch (error) {
+    console.error("SIGNUP_ERROR", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -65,6 +65,6 @@ export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
 
     pages: {
-        signIn: '/login', 
+        signIn: '/signin', 
     }
 };
