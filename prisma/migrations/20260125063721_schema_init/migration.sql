07df5e689aed1fea3e27f7708252ce0a1bb8adcd
Binay <binaydas1123@gmail.com>
2026-01-25 14:33:19 +0530

    migrations

--- prisma/migrations/20260125063721_schema_init/migration.sql
+++ prisma/migrations/20260125063721_schema_init/migration.sql
@@ -24,8 +24,24 @@ CREATE TABLE "public"."PdfSummary" (
     CONSTRAINT "PdfSummary_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."DocumentChunk" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "embedding" vector(768),
+    "pdfSummaryId" TEXT NOT NULL,
+
+    CONSTRAINT "DocumentChunk_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
 
+-- CreateIndex
+CREATE INDEX "DocumentChunk_pdfSummaryId_idx" ON "public"."DocumentChunk"("pdfSummaryId");
+
 -- AddForeignKey
 ALTER TABLE "public"."PdfSummary" ADD CONSTRAINT "PdfSummary_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."DocumentChunk" ADD CONSTRAINT "DocumentChunk_pdfSummaryId_fkey" FOREIGN KEY ("pdfSummaryId") REFERENCES "public"."PdfSummary"("id") ON DELETE CASCADE ON UPDATE CASCADE;
