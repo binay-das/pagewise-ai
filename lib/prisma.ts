4fc7b38f8ff51c6c7d13f847ee322096e39062ed
Binay <binaydas1123@gmail.com>
2026-01-25 14:34:24 +0530

    fix: prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,21 @@
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
+  prisma: PrismaClient | undefined;
+};
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg(pool),
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
